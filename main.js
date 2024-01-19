f78568da68229b34a248c4438813d283f27d032e
Marcos <marcospiderplays@gmail.com>
2024-01-19 10:56:05 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicacao = (a, b) => a * b;
+console.log(multiplicacao(10, 10));
+const concatenacao = (nome) => "Olá " + nome;
+console.log(concatenacao("Marcos"));
---
+++ main.ts
@@ -0,0 +1,7 @@
+const multiplicacao = (a: number, b: number) => a * b;
+
+console.log(multiplicacao(10, 10));
+
+const concatenacao = (nome: string) => "Olá " + nome;
+
+console.log(concatenacao("Marcos"));
