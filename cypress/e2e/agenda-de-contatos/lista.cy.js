4ffe7e696fe996851722ac0eb31d2bf8bd31d0fd
Marcos Mantovani <marcospiderplays@gmail.com>
2024-02-04 17:56:13 -0300

    Consertando autenticações falhas

--- cypress/e2e/agenda-de-contatos/lista.cy.js
+++ cypress/e2e/agenda-de-contatos/lista.cy.js
@@ -12,11 +12,11 @@ describe('Testes para a lista de contatos', () => {
         cy.get('input[type="tel"]').clear().type('48 90000-0000')
         cy.get('button.adicionar').click()
 
-        cy.get('.contato').should('have.length', 1)
+        cy.get('.contato').should('have.length', 4);
     })
 
     it('Deve editar um contato existente', () => {
-        cy.get('.edit').click()
+        cy.get(':nth-child(5) > .sc-gueYoa > .edit').click()
         cy.get('input[type="text"]').clear().type('Pedro')
         cy.get('input[type="email"]').clear().type('pedro@teste.com')
         cy.get('input[type="tel"]').clear().type('48 91111-0000')
@@ -28,8 +28,10 @@ describe('Testes para a lista de contatos', () => {
     })
 
     it('Deve excluir um contato existente', () => {
-        cy.get('button.delete').first().click()
+        cy.get(':nth-child(5) > .sc-gueYoa > .delete').click()
 
-        cy.get('.contato').should('have.length', 0)
+        cy.get('.contato li').should('not.contain', 'Pedro');
+        cy.get('.contato li').should('not.contain', '48 91111-0000');
+        cy.get('.contato li').should('not.contain', 'pedro@teste.com');
     })
 })
